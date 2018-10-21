027103df50210a2dc0aab703d7fac5805eb71a29
Jie Bao <645372879@qq.com>
2018-10-21 20:10:54 +0100

    findall goods endpoint tested

--- test/routes/goods-test.js
+++ test/routes/goods-test.js
@@ -2,10 +2,27 @@ let chai = require('chai');
 let chaiHttp = require('chai-http');
 let server = require('../../bin/www');
 let expect = chai.expect;
-
+chai.use(require('chai-things'));
 chai.use(chaiHttp);
 let _ = require('lodash' );
 
 describe('Goods', function (){
-    // TODO
+    describe('GET /goods',  () => {
+        it('should return all the goods in an array', function(done) {
+            chai.request(server)
+              .get('/goods')
+              .end((err, res) => {
+                  expect(res).to.have.status(200);
+                  expect(res.body).to.be.a('array');
+                  expect(res.body.length).to.equal(8);
+                  let result = _.map(res.body, (goods) => {
+                    return { id: goods.id,
+                        amount: goods.amount } 
+                    });
+                expect(result).to.include( { id: 1001, amount: 1000  } );
+                expect(result).to.include( { id: 1002, amount: 1000  } );
+                done();
+              });
+        });
+    });
 });
